2704c1f482e7527432d19bc1872cfb5a072b3717
CHENNUPALLI LAXMI VARSHITHA <163732058+LaxmiVarshithaCH@users.noreply.github.com>
2025-09-22 22:16:40 +0530

    Update enrollmentService.js

--- src/services/enrollmentService.js
+++ src/services/enrollmentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/enrollments';
+const API_URL = 'http://ec2-56-228-23-27.eu-north-1.compute.amazonaws.com:8080/enrollments';
 
 export const enrollCourse = async (userId, courseId) => {
   const res = await axios.post(`${API_URL}/enroll?userId=${userId}&courseId=${courseId}`);
