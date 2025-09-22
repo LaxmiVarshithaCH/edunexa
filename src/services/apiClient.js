fee4db6b65aeb00b7564bd06bd67b65d7f174058
laxmivarshithach <laxmivarshithach@gmail.com>
2025-09-22 22:36:08 +0530

    v1

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -5,7 +5,7 @@ export const apiClient = axios.create({
 <<<<<<< HEAD
   baseURL: 'http://localhost:8081/auth', // Spring Boot backend
 =======
-  baseURL: 'http://ec2-56-228-23-27.eu-north-1.compute.amazonaws.com:8080/auth', // Spring Boot backend
+  baseURL: 'http://ec2-56-228-23-27.eu-north-1.compute.amazonaws.com:8081/auth', // Spring Boot backend
 >>>>>>> 2704c1f482e7527432d19bc1872cfb5a072b3717
   headers: {
     'Content-Type': 'application/json',
--- src/services/courseService.js
+++ src/services/courseService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 <<<<<<< HEAD
 const API_URL = 'http://localhost:8081/courses';
 =======
-const API_URL = 'http://ec2-56-228-23-27.eu-north-1.compute.amazonaws.com:8080/courses';
+const API_URL = 'http://ec2-56-228-23-27.eu-north-1.compute.amazonaws.com:8081/courses';
 >>>>>>> 2704c1f482e7527432d19bc1872cfb5a072b3717
 
 export const getCourseById = async (id) => {
