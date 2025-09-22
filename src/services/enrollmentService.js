6ddd9271accb728597806e3045598b78a1631dbd
LaxmiVarshithaCH <laxmivarshithach@gmail.com>
2025-09-22 22:43:15 +0530

    v1

--- src/services/enrollmentService.js
+++ src/services/enrollmentService.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
-const API_URL = 'http://localhost:8081/enrollments';
-=======
+
 const API_URL = 'http://ec2-56-228-23-27.eu-north-1.compute.amazonaws.com:8081/enrollments';
->>>>>>> 2704c1f482e7527432d19bc1872cfb5a072b3717
 
 export const enrollCourse = async (userId, courseId) => {
   const res = await axios.post(`${API_URL}/enroll?userId=${userId}&courseId=${courseId}`);
