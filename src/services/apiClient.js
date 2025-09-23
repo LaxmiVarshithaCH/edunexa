cf4201db29e582c62fa53490d66dceee1e29154c
CHENNUPALLI LAXMI VARSHITHA <163732058+LaxmiVarshithaCH@users.noreply.github.com>
2025-09-23 09:40:13 +0530

    Update apiClient.js

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const apiClient = axios.create({
 
-  baseURL: 'http://ec2-56-228-23-27.eu-north-1.compute.amazonaws.com:8081/auth', // Spring Boot backend
+  baseURL: 'http://ec2-56-228-23-27.eu-north-1.compute.amazonaws.com:8081', // Spring Boot backend
 
   headers: {
     'Content-Type': 'application/json',
